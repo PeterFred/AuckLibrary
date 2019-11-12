9a967efbc8a86d427065e84e56962d92bcadfd80
PeterFred <32250775+PeterFred@users.noreply.github.com>
2019-11-12 17:48:36 +1300

    Implemented catalog controller, and catalog models (single and IEnumerable)

---
+++ AuckLibrary/Library/Controllers/CatalogController.cs
@@ -0,0 +1,37 @@
+﻿using LibraryData;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Library.Models.Catalog;
+
+namespace Library.Controllers
+{
+    public class CatalogController : Controller
+    {
+        private ILibraryAsset _assets;
+        public CatalogController(ILibraryAsset assets)
+        {
+            _assets = assets;
+        }
+
+        public IActionResult Index()
+        {
+            var assetModels = _assets.GetAll();
+            var listingResult = assetModels
+                .Select(result => new AssetIndexListingModel
+                {
+                    Id= result.Id,
+                    ImageUrl = result.ImageUrl,
+                    AuthorOrDirector = _assets.GetAuthorOrDirector(result.Id),
+                    Dewey =  _assets.GetDeweyIndex(result.Id),
+                    Type = _assets.GetType(result.Id),
+                    Title = result.Title,
+                });
+
+            var model = new AssetIndexModel()
+            {
+                Assets = listingResult
+            };
+            return View(model);
+        }
+    }
+}
