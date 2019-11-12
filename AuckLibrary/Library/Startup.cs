e67f1646baab37531cbac73ade233fcc743af17a
PeterFred <32250775+PeterFred@users.noreply.github.com>
2019-11-12 14:54:53 +1300

    Added connection string and connectionService

--- AuckLibrary/Library/Startup.cs
+++ AuckLibrary/Library/Startup.cs
@@ -2,12 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LibraryData;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
 
 namespace Library
 {
@@ -24,6 +26,8 @@ namespace Library
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<LibraryContext>(options => options.UseSqlServer(Configuration.GetConnectionString("LibaryConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
