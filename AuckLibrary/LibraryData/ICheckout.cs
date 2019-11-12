0db70c1a9e66b9bb96faf64260c3cf4ed1f42126
PeterFred <32250775+PeterFred@users.noreply.github.com>
2019-11-13 12:10:48 +1300

    Create Icheckout

---
+++ AuckLibrary/LibraryData/ICheckout.cs
@@ -0,0 +1,25 @@
+﻿using LibraryData.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryData
+{
+    interface ICheckout
+    {
+        IEnumerable<Checkout> GetAll();
+        Checkout GetById(int checkoutId);
+        void AddChecout(Checkout checkout);
+        void CheckoutItem(int assetId, int libraryCardId);
+        void CheckInItem(int assetId, int libraryCardId);
+        IEnumerable<CheckoutHistory> GetCheckoutHistoriy(int id);
+
+        void PlaceHold(int assetId, int libraryCardId);
+        string GetCurrentHoldPatronName(int id);
+        DateTime GetCurrentHoldPlaced(int id);
+        IEnumerable<Hold> GetCurrentHolds(int id);
+
+        void MarkLost(int assetId);
+        void MarkFound(int assetId);
+    }
+}
