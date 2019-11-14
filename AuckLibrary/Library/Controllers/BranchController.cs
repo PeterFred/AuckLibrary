4b7d1bc0841198f9278dae7ec6ef958383d8dab6
PeterFred <32250775+PeterFred@users.noreply.github.com>
2019-11-15 09:16:27 +1300

    Added BranchController for library branches
    Need to implement BranchServices in LibraryServices
     and ILibraryBranch in Library Data

---
+++ AuckLibrary/Library/Controllers/BranchController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Controllers
+{
+    public class BranchController : Controller
+    {
+        private ILibraryBranch _branch;
+        public BranchController(ILibraryBranch branch)
+        {
+            branch = _branch;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
