8508b031a1eda91c6a09fa231bfe7f10d469151c
PeterFred <32250775+PeterFred@users.noreply.github.com>
2019-11-15 10:18:19 +1300

    BranchIndexModel and BranchDetailModel created

---
+++ AuckLibrary/Library/Models/Branch/BranchIndexModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Models.Branch
+{
+    public class BranchIndexModel
+    {
+
+        public IEnumerable<BranchIndexModel> Branches { get; set; }
+    }
+}
