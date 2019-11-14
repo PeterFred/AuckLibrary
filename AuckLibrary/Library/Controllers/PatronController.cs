27271da7164230de52e46e454e83c608a962b224
PeterFred <32250775+PeterFred@users.noreply.github.com>
2019-11-14 16:14:43 +1300

    Added PatronDetailModel / PatronIndexModel / updated PatronController with index method

--- AuckLibrary/Library/Controllers/PatronController.cs
+++ AuckLibrary/Library/Controllers/PatronController.cs
@@ -1,5 +1,8 @@
-﻿using LibraryData;
+﻿using Library.Models.Patron;
+using LibraryData;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Library.Controllers
 {
@@ -10,6 +13,29 @@ namespace Library.Controllers
         public PatronController(IPatron patron)
         {
             _patron = patron;
+        }
+
+        public IActionResult Index()
+        {
+            var allPatrons = _patron.GetAll();
+
+            var patronModels = allPatrons.Select(p => new PatronDetailModel
+            {
+                Id = p.Id,
+                FirstName = p.FirstName,
+                LastName = p.LastName,
+                LibraryCardId = p.LibraryCard.Id,
+                Address = p.Address,
+                OverdueFees = p.LibraryCard.Fees,
+                HomeLibraryBranch = p.LibraryBranch.Name
+            }).ToList();
+
+            var model = new PatronIndexModel()
+            {
+                Patrons = patronModels
+            };
+
+            return View(model);
         }
     }
 }
---
+++ AuckLibrary/Library/Models/Patron/PatronDetailModel.cs
@@ -0,0 +1,23 @@
+﻿using LibraryData.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Models.Patron
+{
+    public class PatronDetailModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int LibraryCardId { get; set; }
+        public string Address { get; set; }
+        public DateTime MemberSince { get; set; }
+        public string HomeLibraryBranch { get; set; }
+        public decimal OverdueFees { get; set; }
+        public IEnumerable<Checkout> AssetsCheckedOut { get; set; }
+        public IEnumerable<CheckoutHistory> CheckoutHistory { get; set; }
+        public IEnumerable<Hold> Holds { get; set; }
+    }
+}
